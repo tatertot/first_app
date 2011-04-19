03fb6ea96c7fb019d390a3b9da319f1c72dfb80e
marissa <marissa.marquez@cbs.com>
2011-04-19 21:28:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaridog::Application.config.secret_token = '04bca1248df7c86d653f3f5be021b3f0a916690354cbf663ca5036f95a16f66bd2f1e8794cd4a5057436bc11153a3607587031f2228e3ddd5171d4cdd3d49f27'
